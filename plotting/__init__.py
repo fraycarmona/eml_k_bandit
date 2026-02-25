3237b5873586de2f2aca5ee958ccc042a546eef0
fraycarmona <adrian.r@um.es>
2026-02-25 18:53:35 +0100

    Update imports and public modules in __init__.py

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -13,8 +13,14 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 """
 
 # Importación de módulos o clases
-from .plotting import plot_average_rewards, plot_optimal_selections
+from .plots import plot_average_rewards, plot_optimal_selections
+from .comparison import compare_best_temperatures, compute_scores
 
 # Lista de módulos o clases públicas
-__all__ = ['plot_average_rewards', 'plot_optimal_selections']
+__all__ = [
+    'plot_average_rewards',
+    'plot_optimal_selections',
+    'compare_best_temperatures',
+    'compute_scores',
+]
 
